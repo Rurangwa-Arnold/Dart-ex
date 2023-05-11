5b041454a942eea2cc224a7bddf1d2ced2a1534a
Rurangwa arnold <81275837+Rurangwa-Arnold@users.noreply.github.com>
2023-05-11 14:06:27 +0200

    Add files via upload

---
+++ 1st task.dart
@@ -0,0 +1,10 @@
+import 'dart:ffi';
+import 'dart:js_util';
+
+Dart main.Dart
+void main(list<string> args){
+  String firstname = args[0];
+  String lastname = args[1];
+  DateTime currenttime = DateTime now();
+  Print ("Hello,$ {firtsname}, $ {lastname}, $ {currenttime}s")
+}
